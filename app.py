c73c97655ec6344b7823a6f262f7d7dfa34ce915
clickTwice26 <shagatoc@gmail.com>
2024-01-01 19:57:09 +0600

    class 1 completed

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ app = Flask(__name__) #helps find all of our files and directory
 
 
 
+# Create a route decorator
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/user/<username>")
+def user(username):
+    return f"<h1>{username}</h2>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
