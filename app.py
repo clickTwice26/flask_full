b82538e12196fc815e24ab90cb1e03cb6eca9221
clickTwice26 <shagatoc@gmail.com>
2024-01-01 19:23:05 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, redirect
+
+
+
+app = Flask(__name__) #helps find all of our files and directory
+
+
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+##FLASK
+(tutorial)[https://www.youtube.com/playlist?list=PLCC34OHNcOtolz2Vd9ZSeSXWc8Bq23yEz]
