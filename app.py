77b0f7947433573856e5bf0ec52d199d71851ee1
clickTwice26 <shagatoc@gmail.com>
2024-01-02 04:23:04 +0600

    class 2 completed

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, request, redirect
 
-
-
+tutorial_link="https://www.youtube.com/watch?v=0Qxtt4veJIc&list=PLCC34OHNcOtolz2Vd9ZSeSXWc8Bq23yEz"
 app = Flask(__name__) #helps find all of our files and directory
-
-
-
 # Create a route decorator
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    first_name = "John"
+    stuff = "This is <strong> Bold </strong>"
+    item_list = ["item1", "item2", "item3", 41, 41]
+    return render_template("index.html", first_name=first_name, stuff=stuff, item_list=item_list)
 @app.route("/user/<username>")
 def user(username):
-    return f"<h1>{username}</h2>"
+    return render_template("user.html", username=username)
 
 if __name__ == "__main__":
     app.run(debug=True)
