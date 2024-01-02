8d9e065615deb734166f01def985dbeefc7105b3
clickTwice26 <shagatoc@gmail.com>
2024-01-02 21:37:25 +0600

    class 5 completed

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 tutorial_link="https://www.youtube.com/watch?v=0Qxtt4veJIc&list=PLCC34OHNcOtolz2Vd9ZSeSXWc8Bq23yEz"
 app = Flask(__name__) #helps find all of our files and directory
-# Create a route decorator
+app.config['SECRET_KEY'] = 'chipichipichapachapadubidubudabadaba'
 
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")# Create a route decorator
 @app.route("/")
 def index():
     first_name = "John"
@@ -19,5 +25,20 @@ def page_not_found(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template("500.html"), 500
+
+
+#create name page
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+    form = NamerForm()
+    #validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form=form)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 ##FLASK
 (tutorial)[https://www.youtube.com/playlist?list=PLCC34OHNcOtolz2Vd9ZSeSXWc8Bq23yEz]
+
+WTF-Form:https://flask-wtf.readthedocs.io/en/1.2.x/
